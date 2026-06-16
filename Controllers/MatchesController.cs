626d93888dd291d7426a2fadd296b1f7b396f9e9
EnzoTlz <enzo31del@gmail.com>
2026-06-16 10:32:40 +0200

    ajout de la vue pour afficher la liste des matches

---
+++ Controllers/MatchesController.cs
@@ -0,0 +1,23 @@
+﻿using Eval.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Eval.Controllers
+{
+    public class MatchesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MatchesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var matches = await _context.Matches.OrderBy(m => m.KickoffUtc).ToListAsync();
+
+            return View(matches);
+        }
+    }
+}
