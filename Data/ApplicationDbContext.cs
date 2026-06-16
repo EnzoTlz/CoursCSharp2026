2b1c8ed2f7e5ee1ddea2a7be8e076760093e3465
EnzoTlz <enzo31del@gmail.com>
2026-06-16 09:48:54 +0200

    init projet

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Eval.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
